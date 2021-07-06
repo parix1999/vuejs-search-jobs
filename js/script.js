ffe577af788e379f387c2a133c97440956e33be6
parix1999 <parisottoedoardo@gmail.com>
2021-07-06 17:04:35 +0200

    job

--- js/script.js
+++ js/script.js
@@ -45,6 +45,16 @@ new Vue(
                     city: 'Roma',
                     contract: 'Full Time'
                 },
+                {
+                    id: 5,
+                    company: 'webBuonasera',
+                    position: 'Developer-medium',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
                   
             ],
             starred: [1, 2, 3],
@@ -54,6 +64,7 @@ new Vue(
         methods: {
             checkStarIndex: function (index) {
                 if (this.starred.includes(index)){
+                    console.log(index)
                     return "fas fa-star";
                 } else {
                     return "far fa-star";
@@ -63,10 +74,14 @@ new Vue(
                 if (!this.starred.includes(index)){
                     this.starred.push(index);
                     
-                } else {
-                    this.starred.splice(index,1);
-                    // ToDo, vedere di far funzionare l'eliminazione della stella;
                 }
+            },
+            applayList: function (index) {
+                if (this.applied.includes(index)) {
+                    return 
+
+                }
+
             },
         },
 
