29611d74f404e0c2700427303e3f1314ae031273
parix1999 <parisottoedoardo@gmail.com>
2021-07-06 11:42:07 +0200

    milestone-1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,31 @@ new Vue(
     {
         el:'#app',
         data:{
-
+            jobs: [
+                {
+                  id: 1,
+                  company: 'Perferendis',
+                  position: 'Developer',
+                  description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                  created_at: '5/22/2021',
+                  logo: 'logo.jpg',
+                  city: 'Roma',
+                  contract: 'Full Time'
+                },
+                {
+                    id: 2,
+                    company: 'Perferendis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                  },
+                  
+            ],
+            starred: [1, 2, 3],
+            applied: [4, 5]
             
         },
 
